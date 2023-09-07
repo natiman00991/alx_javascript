21b64fa49f23da5ac7e043d3af6cc97cefa4551c
natiman <natiabnew@gmail.com>
2023-09-07 13:00:59 -0700

    ffffffffffffffffffffff

---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function callMeMoby(x, theFunction) {
+  if (x > 0) {
+    theFunction();
+    callMeMoby(x - 1, theFunction);
+  }
+}
+
+module.exports = {
+  callMeMoby: callMeMoby,
+};
