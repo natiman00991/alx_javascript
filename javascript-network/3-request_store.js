b1688e68c6c1e2d939dc684987970caed65c9a50
natiman <natiabnew@gmail.com>
2023-09-13 23:21:25 -0700

    zzzzzzzzzzzzzzz

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,21 @@
+const request = require("request");
+const fs = require("fs");
+
+const url = process.argv[2]; // Get the URL from the command line arguments
+const filePath = process.argv[3]; // Get the file path to store the response
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error making the request: ${error.message}`);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Request failed with status code ${response.statusCode}`);
+    return;
+  }
+
+  // Write the body of the response to the specified file
+  fs.writeFileSync(filePath, body, "utf-8");
+  console.log(`Response saved to ${filePath}`);
+});
