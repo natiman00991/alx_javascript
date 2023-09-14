531583aa6887bf5504da44a42d14305a4270c1c3
natiman <natiabnew@gmail.com>
2023-09-13 23:05:31 -0700

    gg

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,20 +1,13 @@
-const axios = require("axios");
+const request = require("request");
 
 const url = process.argv[2]; // Get the URL from the command line arguments
 
-axios
-  .get(url)
-  .then((response) => {
-    // Print the status code
-    console.log(`code: ${response.status}`);
-  })
-  .catch((error) => {
-    // Handle errors, including HTTP request errors
-    if (error.response) {
-      // The request was made, but the server responded with a status code outside of the 2xx range
-      console.error(`code: ${error.response.status}`);
-    } else {
-      // There was an error in making the request
-      console.error("Error making the request:", error.message);
-    }
-  });
+request(url, (error, response) => {
+  if (error) {
+    console.error(`Error making the request: ${error.message}`);
+    return;
+  }
+
+  // Print the status code
+  console.log(`code: ${response.statusCode}`);
+});
